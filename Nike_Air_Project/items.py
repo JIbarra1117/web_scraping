59ce0b7cae167bfda29cd454b8d066fd45e65a1f
JLIbarra1117 <jorgeluisibarra_lucas@outlook.com>
2023-10-18 09:37:07 -0500

    Extraccion a pagina de Nike 18/10/2023

---
+++ Nike_Air_Project/items.py
@@ -0,0 +1,20 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ZapatillaItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    modelo = scrapy.Field()
+    marca = scrapy.Field()
+    tallas = scrapy.Field()
+    color = scrapy.Field()
+    precio = scrapy.Field()
+    imagenes = scrapy.Field()
+    tipo = scrapy.Field()
+    url_raiz = scrapy.Field()
+    url_calzado = scrapy.Field()
